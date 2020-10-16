1de2182e8236fd0bbf81177a1fbb61878e608cde
Roberto Rodriguez <roberto.rodriguez@patelhs.org>
2020-10-16 22:16:17 +0000

    adding scanner

--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
 
 import java.util.Scanner;
 import java.util.Random;
+
 class Main {
   public static void main(String[] args) {
     //start here
     int answerOne;
     Scanner scan = new Scanner(System.in);
-    System.out.println("Welcome to 8 ball\n\n");
-    System.out.println("What is your question?");
-    answerOne = scan.nextInt;
-    
+    System.out.println("\nWelcome to 8 ball\n\n");
+    System.out.println("What is your yes/no question?\n");
+    answerOne = scan.nextInt();
+    Random rand = new Random();
+    int value = rand.nextInt(3-1) + 1;
+    if(value == 1) {
+      System.out.print("Yes");
+    }
+    else if(value == 2) {
+      System.out.print("Maybe");
+    }
+    else if(value == 3) {
+      System.out.print("No");
+    }
     //end here
   }
 }
