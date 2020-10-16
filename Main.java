0124e0133684a077be9b7df0b736244cdf520f12
Roberto Rodriguez <roberto.rodriguez@patelhs.org>
2020-10-16 22:39:00 +0000

    added while

--- Main.java
+++ Main.java
@@ -5,22 +5,33 @@ import java.util.Random;
 class Main {
   public static void main(String[] args) {
     //start here
-    int answerOne;
-    Scanner scan = new Scanner(System.in);
     System.out.println("\nWelcome to 8 ball\n\n");
-    System.out.println("What is your yes/no question?\n");
-    answerOne = scan.nextInt();
-    Random rand = new Random();
-    int value = rand.nextInt(3-1) + 1;
-    if(value == 1) {
-      System.out.print("Yes");
-    }
-    else if(value == 2) {
-      System.out.print("Maybe");
-    }
-    else if(value == 3) {
-      System.out.print("No");
-    }
+    boolean gameStart = true;
+
+    while(gameStart = true) {
+      String answerOne;
+      Scanner scan = new Scanner(System.in);
+      System.out.println("What is your yes/no question?\n");
+      answerOne = scan.nextLine();
+      Random rand = new Random();
+      int value = rand.nextInt(3-1) + 1;
+      if(value == 1) {
+        System.out.print("\nYes\n");
+      }
+      else if(value == 2) {
+        System.out.print("\nMaybe\n");
+      }
+      else if(value == 3) {
+        System.out.print("\nNo\n");
+      }
+      System.out.println("\nPress 1 to go back to the main screen");
+      String retry = scan.nextLine();
+      String retryIs = "1";
+      if(retry.equals(retryIs)) {
+        gameStart = false;
+        gameStart = true;
+      }
     //end here
+    }
   }
 }
