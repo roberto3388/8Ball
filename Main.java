2fd8f95258c6a8263331c7ff192bdd8081b8ba5d
Roberto Rodriguez <roberto.rodriguez@patelhs.org>
2020-10-16 19:25:42 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+import java.util.Random;
+class Main {
+  public static void main(String[] args) {
+    //start here
+    int answerOne;
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Welcome to 8 ball\n\n");
+    System.out.println("What is your question?");
+    answerOne = scan.nextInt;
+    
+    //end here
+  }
+}
